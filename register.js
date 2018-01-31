4f6a4b6ec8fc1663b102ec30ff0edb2c41f1fab1
Jônatas Renan <jonatas@dcc.ufmg.br>
2018-01-31 11:11:53 -0200

    Initial plugin commit

---
+++ register.js
@@ -0,0 +1,11 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "multi-domain",
+  name: "multi-domain",
+  icon: "fa fa-random",
+  autoEnable: true,
+  settings: {},
+  registry: []
+});
+
